6c1869078705b7a2ac5a8a4d0d15ec3c31560fb0
MikyFit <mikyfiteu@gmail.com>
2022-06-22 16:44:43 +0200

    Creating Playwright's locators
    At first, I need to create Playwright's locators to find elements on the page.
    
    I am going to select elements based on the page layout.

--- index.js
+++ index.js
@@ -31,8 +31,24 @@ const get_binance_pair_price = async (pair) => {
 
         await page.goto('https://app.hector.finance', { waitUntil: 'networkidle' });
 
+        // I need to create Playwright's locators to find elements on the page. I am going to select elements based on the page layout. 
+        const marketCap = await page.locator('div:below(:text("Market Cap")) >> nth=0').innerText();
+        const hecPrice = await page.locator('div:below(:text("Hec Price")) >> nth=0').innerText();
+        const hecBurned = await page.locator('div:below(:text("Hec Burned")) >> nth=0').innerText();
+        const circulatingSupply = await page.locator('div:below(:text("Circulating Supply")) >> nth=0').innerText();
+        const hecTreasury = await page.locator('div:below(:text("Treasury")) >> nth=0').innerText();
+        const currentIndex = await page.locator('div:below(:text("Current Index")) >> nth=0').innerText();
+
         console.log('   BTC Price:', '$' + btcPrice)
         console.log('   FTM Price:', '$' + ftmPrice)
+
+        console.log('   Market Cap:', '$' + marketCap)
+        console.log('   HEC Price:', '$' + hecPrice)
+        console.log('   HEC Burned:', hecBurned)
+        console.log('   Circulating Supply:', circulatingSupply)
+        console.log('   HEC Treasury:', '$' + hecTreasury)
+        console.log('   Current Index:', currentIndex)
+
         console.log('==>Scraping from app.hector.finance finished');
         console.log('');
 
