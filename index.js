eacef349260f05c6f57aba8b561c1e8c68d7aebb
MikyFit <mikyfiteu@gmail.com>
2022-06-22 16:29:10 +0200

    Read btc and ftm prices from api.binance.com
    Get current token price from api.binance.com for BTC, and FTM

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const { chromium } = require("playwright");
 const axios = require('axios');
-const fs = require("fs");
+
+// Get current token price from api.binance.com
+const get_binance_pair_price = async (pair) => {
+    const api = `https://api.binance.com/api/v3/ticker/price?symbol=${pair}`
+    const response = await axios.get(api)
+
+    return response.data
+}
 
 // Main func run Playwright ...
 (async () => {
@@ -10,11 +17,24 @@ const fs = require("fs");
         await page.setViewportSize({ "width": 880, "height": 1000 });
         await page.setDefaultTimeout(50000);
 
+        // Reading current token prices from api.binance.com
+        const BTCUSDT = await get_binance_pair_price("BTCUSDT")
+        const btcPrice = parseFloat(BTCUSDT['price']).toFixed(2)
+        const FTMUSDT = await get_binance_pair_price("FTMUSDT")
+        const ftmPrice = parseFloat(FTMUSDT['price']).toFixed(4)
+
+
+
         // Scraping data from app.hector.finance
         console.log('==>Scraping data from app.hector.finance');
         console.log('==>goto app.hector.finance');
 
         await page.goto('https://app.hector.finance', { waitUntil: 'networkidle' });
+
+        console.log('   BTC Price:', '$' + btcPrice)
+        console.log('   FTM Price:', '$' + ftmPrice)
+        console.log('==>Scraping from app.hector.finance finished');
+        console.log('');
 
         // Closing app
         await browser.close();
